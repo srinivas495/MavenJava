76aeb3ea7f19ad8e780e8a9e620c9df352da12ea
Srinivas <srinivaschari.y@gmail.com>
2021-02-08 20:58:42 +0530

    MavenJava

---
+++ src/test/java/SrinivasMavenProject/AppiumTest.java
@@ -0,0 +1,19 @@
+package SrinivasMavenProject;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void PostJIRA() 
+	{
+		System.out.println("PostJIRA");
+	}
+	
+	@Test
+	public void deleteTwiter() 
+	{
+		System.out.println("deleteTwiter");
+	}
+
+}
---
+++ src/test/java/SrinivasMavenProject/RESTAPITest.java
@@ -0,0 +1,19 @@
+package SrinivasMavenProject;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void NativeAppAndriod() 
+	{
+		System.out.println("NativeAppAndriod");
+	}
+	
+	@Test
+	public void IOSAppTest() 
+	{
+		System.out.println("IOSAppTest");
+	}
+
+}
